f896be9c9ba991273aa3d3c47ef41a3934d39b4f
ngha79 <nguyenmha07092003@gmail.com>
2023-03-25 23:48:41 +0700

    update

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export let socket = io("https://mernappsocialmedia.onrender.com", {
+export let socket = io(process.env.REACT_APP_API_URL, {
   transports: ["websocket"],
   withCredentials: true,
   extraHeaders: {
